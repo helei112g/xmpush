041a90eb87979aea86d26f7acb8d3cf4764ffe7e
helei112g <helei5200@126.com>
2016-11-22 16:35:36 +0800

    关闭小米的输出信息

--- sdk/HttpBase.php
+++ sdk/HttpBase.php
@@ -80,8 +80,10 @@ class HttpBase {
 	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
-	    print_r('HTTP Params <br> '.urldecode(http_build_query($fields)));
-	    echo'<br>';
+
+		// 调试代码，直接关闭
+	    //print_r('HTTP Params <br> '.urldecode(http_build_query($fields)));
+	    //echo'<br>';
 	    // Execute post
 	    $result = curl_exec($ch);
 	    	
